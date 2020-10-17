eda5ba9a93ca791a92b52e1f14ccc9650371888e
pietro2356 <pietro.rocchio@gmail.com>
2020-10-17 20:40:45 +0200

    Implementata la classe Download.

---
+++ AirportChartsManager/CLAirportChartsManager/Downlaod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace CLAirportChartsManager
+{
+    public class Downlaod
+    {
+        private WebClient webClient = new WebClient();
+        private static string url = "https://charts.avbot.in/";
+        private string ICAO = "";
+
+
+        public Downlaod(string ICAO)
+        {
+            this.ICAO = ICAO;
+        }
+        public Downlaod() { }
+
+
+        public void DownloadFile()
+        {
+            webClient.DownloadDataAsync(new Uri(url + ICAO + ".pdf"));
+        }
+    }
+}
